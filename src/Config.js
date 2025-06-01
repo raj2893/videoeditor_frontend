7f9ee1aaa8b7a371d0120d7b1dbf4bc32a9682fe
rajp4 <raj.p@cloudoffis.com.au>
2025-06-01 21:41:26 +0530

    ProjectEditor add to timeline changes

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,6 @@
 // export const API_BASE_URL = "http://localhost:8080";
-export const API_BASE_URL = "https://videoeditor-app.onrender.com";
+// export const API_BASE_URL = "https://videoeditor-app.onrender.com";
+export const API_BASE_URL = "https://scenith.onrender.com";
 
 export const CDN_URL = "https://cdn.scenith.in";
 // export const CDN_URL = "http://localhost:8080";

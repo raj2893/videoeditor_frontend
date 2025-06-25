d91cd91cb79a699de217ae59e40973bf6d21af7b
rajp4 <raj.p@cloudoffis.com.au>
2025-06-25 16:30:09 +0530

    AWS Testing

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,7 @@
 // export const API_BASE_URL = "https://videoeditor-app.onrender.com";
 // export const API_BASE_URL = "http://localhost:8080";
-export const API_BASE_URL = "https://scenith.onrender.com";
+// export const API_BASE_URL = "https://scenith.onrender.com";
+export const API_BASE_URL = "http://52.66.104.242:1000";
 
 export const CDN_URL = "https://cdn.scenith.in";
 // export const CDN_URL = "http://localhost:8080";
